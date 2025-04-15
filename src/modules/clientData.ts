deb321f825fea355ea1e93899d31a4f6b16b471c
animeduniya <duniyabarbaad24@gmail.com>
2025-04-15 15:01:59 +0530

    Create clientData.ts

---
+++ src/modules/clientData.ts
@@ -0,0 +1,8 @@
+# Example:
+import { ClientData } from "../types/types";
+
+export const clientData: ClientData = {
+  clientId: 22150,
+  redirectUri: 'akuse://index',
+  clientSecret: '7eHYdGMe6dsYs9DOZkvN9jTKko2WOn3YBq3p2aCR',
+};
